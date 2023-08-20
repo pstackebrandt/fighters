fcb083642edfd1fbcdc0170252c72c682b9a7a9f
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-20 20:06:26 +0200

    Player: Add getAttackValue(), attack() Add strict type check and change class accordingly

--- Player.php
+++ Player.php
@@ -1,24 +1,37 @@
 <?php
 
-class Player {
+declare(strict_types=1);
+
+class Player
+{
    public $name;
    protected $health = 100;
    protected $strength = 7;
 
-   function __construct($name)
+   function __construct(string $name)
    {
       $this->name = $name;
    }
 
-   function getHealth()
+   function getHealth(): int
    {
       return $this->health;
    }
 
-   function getStrength()
+   function getStrength(): int
    {
       return $this->strength;
    }
-}
 
-?>
+   function getAttackValue(): int
+   {
+      return $this->getStrength();
+   }
+
+   function attack(int $value): string
+   {
+      $this->health -= $value;
+
+      return "$this->name hat $value Gesundheit verloren.";
+   }
+}
